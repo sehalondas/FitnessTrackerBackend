cfee00e152cbfb49a9aa557621b0c40b7a384772
Brandon Johnson <developmentbybjohnson@gmail.com>
2023-07-19 19:53:53 -0500

    Created tables and created create user function

--- db/users.js
+++ db/users.js
@@ -4,24 +4,33 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
+  const { rows: user } = await client.query(
+    `
+  INSERT INTO users (username, password)
+  VALUES ($1, $2),
+  ON CONFLICT (username) DO NOTHING,
+  RETURNING *; 
   
+  `,
+    [username, password]
+  );
+  return user;
 }
 
 async function getUser({ username, password }) {
-
+  const { rows } = await client.query(`
+  SELECT * FROM users,
+  WHERE username = $1
+  `);
 }
 
-async function getUserById(userId) {
+async function getUserById(userId) {}
 
-}
-
-async function getUserByUsername(userName) {
-
-}
+async function getUserByUsername(userName) {}
 
 module.exports = {
   createUser,
   getUser,
   getUserById,
   getUserByUsername,
-}
+};
